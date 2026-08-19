6137cd18773489f1789629e4446b7da80e894864
wuweiluohua <32405441+wuweiluohua@users.noreply.github.com>
2026-08-19 23:02:44 +0800

    Fix formatting in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
-    implementation("androidx.activity:activity-compose:1.9.1)
+    implementation("androidx.activity:activity-compose:1.9.1")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
